628b0129e6d2b6c7ebf5efdd3d1d3e06fcd33930
YakushinSar <saratov482043162@gmail.com>
2025-04-02 13:34:52 +0400

    [+] lesson7

---
+++ src/test/java/lesson7/SeleniumShadowDomTests.java
@@ -0,0 +1,37 @@
+package lesson7;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static lesson7.Constants.BASE_URL;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SeleniumShadowDomTests {
+    WebDriver driver;
+
+    @BeforeEach
+    void start() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void testShadowDom() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/shadow-dom.html");
+
+        assertThrows(NoSuchElementException.class, () -> driver.findElement(By.cssSelector("p")));
+        WebElement content = driver.findElement(By.id("content"));
+        SearchContext shadowRoot = content.getShadowRoot();
+        WebElement textElement = shadowRoot.findElement(By.cssSelector("p"));
+        assertThat(textElement.getText()).contains("Hello Shadow DOM");
+    }
+}
